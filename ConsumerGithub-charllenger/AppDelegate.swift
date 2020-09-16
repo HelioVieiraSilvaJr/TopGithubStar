85bf0e094fc1618ef80a04e67656cf67d26d4f1a
Helio Junior <helio.engenharia@gmail.com>
2020-09-16 13:13:15 -0300

    first commit, prepare environment.

---
+++ ConsumerGithub-charllenger/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  ConsumerGithub-charllenger
+//
+//  Created by Helio Junior on 16/09/20.
+//  Copyright © 2020 HelioTecnologia. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        
+        
+        return true
+    }
+
+}
+
